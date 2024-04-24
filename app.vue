4596a1271196ab3988c99507e599ba21959e4464
DewminK <dewminkasmitha30@gmail.com>
2024-04-24 18:41:37 +0530

    Nuxt creation

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    
+    <ul>
+      <li>Day 01</li>
+      <li>Day 01</li>
+      <li>Day 03</li>
+    
+    </ul>'
+    <h1>Dewmin/h1>
+    <script>
+    </script>
+  </div>
+</template>
