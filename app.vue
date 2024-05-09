d117e112cb72d00812f2eaefdcf72693f4b1378f
DewminK <dewminkasmitha30@gmail.com>
2024-05-09 20:46:52 +0530

    Created the backend

--- app.vue
+++ app.vue
@@ -6,9 +6,14 @@
       <li>Day 01</li>
       <li>Day 03</li>
     
-    </ul>'
+    </ul>
     <h1>Dewmin/h1>
     <script>
+      const name ="Dewmin";
+      const {data:projects, pending, error}= usefetch('http://localhost:5000/projects')
     </script>
   </div>
 </template>
+
+<style>
+</style>
