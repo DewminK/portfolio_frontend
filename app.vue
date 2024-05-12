50e451af6af116df729bdf94351aa7984210d8b6
DewminK <dewminkasmitha30@gmail.com>
2024-05-12 20:35:45 +0530

    Backend Integration

--- app.vue
+++ app.vue
@@ -1,19 +1,43 @@
 <template>
   <div>
-    
-    <ul>
-      <li>Day 01</li>
-      <li>Day 01</li>
-      <li>Day 03</li>
-    
-    </ul>
-    <h1>Dewmin/h1>
-    <script>
-      const name ="Dewmin";
-      const {data:projects, pending, error}= usefetch('http://localhost:5000/projects')
-    </script>
+    <h1>I am {{ name }}</h1>
+
+    <div v-if="pendingProjects">Loading projects...</div>
+    <div v-else>
+      <h2>Projects</h2>
+      <ul>
+        <li v-for="project in projects" :key="project.name">
+          <h3>{{ project.name }}</h3>
+          <p>{{ project.description }}</p>
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="pendingBlogs">Loading blogs...</div>
+    <div v-else>
+      <h2>Blogs</h2>
+      <ul>
+        <li v-for="blog in blogs" :key="blog.title">
+          <h3>{{ blog.title }}</h3>
+          <p>{{ blog.content }}</p>
+          <p>{{ blog.date }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<style>
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import useFetch from './useFetch'; 
+
+  const name = "Dewmin";
+
+  const { data: projects, pending: pendingProjects, error: errorProjects } = useFetch("http://localhost:5000/projects");
+
+  const { data: blogs, pending: pendingBlogs, error: errorBlogs } = useFetch("http://localhost:5000/blogs");
+</script>
+
+
+<style scoped>
 </style>
